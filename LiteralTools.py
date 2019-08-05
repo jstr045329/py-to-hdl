993242d1c833940c765f8d716e591625dd3c6908
James Strieter <jamesstrieter@MacBook-Air-2.local>
2019-08-05 13:46:29 -0700

    Added automatic underscore generation

--- LiteralTools.py
+++ LiteralTools.py
@@ -1,7 +1,21 @@
 """Contains tools to generate literals in VHDL"""
 
 
-def bin_literal(num_digits, n=1):
+def insert_underscore(some_str, idx):
+    _, remainder = divmod(len(some_str) - idx, 4)
+    return remainder == 0 and idx > 0
+
+
+def underscorify(y, prefix):
+    z = ''
+    for i in range(len(y)):
+        if insert_underscore(y, i):
+            z += "_"
+        z += y[i]
+    return prefix + z + '"'
+
+
+def bin_literal(num_digits, n=1, use_underscores=True):
     """Like hex_literal(), but with single quotes."""
     y = bin(n)
     y = y[2:]
@@ -9,11 +23,12 @@ def bin_literal(num_digits, n=1):
         num_zeros = num_digits - len(y)
         zero_str = "0" * num_zeros
         y = zero_str + y
-    # todo: insert underscores
+    if use_underscores:
+        return underscorify(y, 'B"')
     return 'B"' + y + '"'
 
 
-def hex_literal(num_digits, n=1):
+def hex_literal(num_digits, n=1, use_underscores=True):
     """Like bin_literal(), but with double quotes."""
     y = hex(n)
     y = y[2:]
@@ -21,7 +36,8 @@ def hex_literal(num_digits, n=1):
         num_zeros = num_digits - len(y)
         zero_str = "0" * num_zeros
         y = zero_str + y
-    # todo: insert underscores
+    if use_underscores:
+        return underscorify(y, 'X"')
     return 'X"' + y + '"'
 
 
@@ -35,4 +51,11 @@ def all_ones_hex(num_digits):
 
 if __name__ == "__main__":
     print(bin_literal(64, 2934872938))
+    print(bin_literal(7, 1, use_underscores=False))
+    print(bin_literal(7, 1, use_underscores=True))
+    print(bin_literal(11, 72, use_underscores=False))
+    print(bin_literal(11, 72, use_underscores=True))
     print(hex_literal(32, 93920394820394802934802934802394))
+    print(hex_literal(33, 93920394820394802934802934802394))
+    print(hex_literal(34, 93920394820394802934802934802394))
+    print(hex_literal(35, 93920394820394802934802934802394))
